ca53278419317bfe2f97870e6a7cb82547e0a511
Joepieler <be.campagne@live.nl>
2020-03-26 22:04:16 +0100

    made opdracht 3

--- Opdracht3.py
+++ Opdracht3.py
@@ -1,68 +1,53 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-def sigmoid(x):
-  return 1 / (1+np.exp(-x))
+def sigmoid(x, derivate=False):
+  if(derivate==True):
+    return (x * (1-x))
 
-def sigmoid_derivate(x):
-    return x * (1 * x)
-
-input_vector = np.array([[0,0,1],
-                         [1,1,1],
-                         [1,0,1],
-                         [0,1,1]])
-np.random.seed(1)
-
-output = np.array([[0,1,1,0]]).T
-
-synaptic_weights = 2 * np.random.random((3, 1)) -1
+  return 1 / (1 + np.exp(-x))
 
 
-class NeuralNetwork:
-
-    def __init__(self, inputSize, numberHiddenLayers, sizeHiddenLayers, numberOfOuputs ):
-        self.hidden_neurons = [[Neuron(inputSize) for i in range(sizeHiddenLayers)]for i in range(numberHiddenLayers)]
-        self.output_neurons = [Neuron(len(self.hidden_neurons)) for i in range(numberOfOuputs)]
-
-    def calc(self, input):
-        for i in range(len(self.hidden_neurons)):
-            calc_output = [self.hidden_neurons[i][j].calc(input) for j in range(len(self.hidden_neurons[0]))]
-        return [i.calc(calc_output) for i in self.output_neurons]
-
-    def train(self, input, ActualOutput, iterations):
-        for i in range(iterations):
-            for j in range(len(self.output_neurons)):
-                calc_output = self.calc(input)
-                error = ActualOutput - calc_output
-                #adjustments = error * sigmoid_derivate(calc_output)
+#fuctions for the iris assigment
+def name_to_array(x):
+  if x == b'Iris-setosa':
+    return np.array([1,0,0])
+  elif x == b'Iris-versicolor':
+    return np.array([0,1,0])
+  elif x == b'Iris-virginica':
+    return np.array([0,0,1])
 
 
+def nomelize(data):
+  for j in range(len(data[0])):
+    lst = [i[j] for i in data]
+    highest = max(lst)
+    lowest = min(lst)
+    for i in data:
+      i[j] -= lowest
+      i[j] = (i[j]/highest)
+  return data
 
 
-class Neuron:
+def probability(output, actoalouput):
+  xcorrect = 0
+  for i in range(len(output)):
+    if np.where(output[i] == max(output[i])) == np.where(actoalouput[i] == max(actoalouput[i])):
+     xcorrect += 1
+  return  xcorrect
 
-    def __init__(self, NumberOfInput):
-        self.weight = 2 * np.random.random((NumberOfInput, 1)) -1
-        self.Bias = 0
-        self.threshold = 0.5
+def dataReader(file):
+  return nomelize(np.genfromtxt(file, delimiter=",", usecols=[0, 1, 2, 3]))
 
-    def calc(self, input):
-        return sigmoid(np.dot(input, self.weight))
 
-    def train(self, input, ActualOutput, iterations):
-        for i in range(iterations):
-            calc_output = self.calc(input)
-            error = ActualOutput - calc_output
-            adjustments = error * sigmoid_derivate(calc_output)
-            self.weight += np.dot(input.T, adjustments)
-
-    def print(self):
-        for i in range(len(self.weight)):
-            print(self.weight[i])
+def nameReader(file):
+  return np.genfromtxt(file, delimiter=",", usecols=[4], converters={4: lambda x: name_to_array(x)})
 
 
 
-'''setup of gates '''
+'''4.1 NOR gate 3 inputs'''
+print ("opdracht 4.1")
+
+#setup gates
 NOR_input = np.array([[0,0,0],
                      [0,0,1],
                      [0,1,0],
@@ -73,60 +58,153 @@ NOR_input = np.array([[0,0,0],
                      [1,1,1]])
 NOR_output = np.array([[1,0,0,0,0,0,0,0]]).T
 
-XOR_input = np.array([[0,0],
-                      [0,1],
-                      [1,0],
-                      [1,1]])
-XOR_output = np.array([[0,1,1,0]]).T
 
-'''4.1 NOR gate 3 inputs'''
+class Neuron:
+
+  def __init__(self, NumberOfInput):
+    self.weight = 2 * np.random.random((NumberOfInput, 1)) - 1
+    self.Bias = 0
+    self.threshold = 0.5
+
+  def setWeight(self, weight):
+    for i in range(len(weight)):
+      self.weight[i] = weight[i]
+
+
+  def calc(self, input):
+    return sigmoid(np.dot(input, self.weight))
+
+  def train(self, input, ActualOutput, iterations):
+    for i in range(iterations):
+      calc_output = self.calc(input)
+      error = ActualOutput - calc_output
+      adjustments = error * sigmoid(calc_output, True)
+      self.weight += np.dot(input.T, adjustments)
+
+  def print(self):
+    for i in range(len(self.weight)):
+      print(self.weight[i])
+
 NOR_gate = Neuron(3)
-#NOR_gate.setWeight()
+NOR_gate.setWeight([-1, -1, -1])
+print(NOR_gate.calc(NOR_input))
+NOR_gate.print()
 
 '''4.2 Neural ADDER'''
 
 
-'''4.3 XOR Gate 2 inputs'''
+'''4.3 XOR Gate 2 inputs
+I \/H\
+  ||  > O
+I /\H/
 
-HiddenNeuron0 = Neuron(2)
-HiddenNeuron1 = Neuron(2)
-OutputNeuron = Neuron(2)
-
-for i in XOR_input:
-    print(OutputNeuron.calc(np.array([HiddenNeuron0.calc(i), HiddenNeuron1.calc(i)]).T))
-
-for t in range(100):
-    for i in range(len(XOR_input)):
-        OutputNeuron.train(np.array([HiddenNeuron0.calc(XOR_input[i]),
-                                     HiddenNeuron1.calc(XOR_input[i])]).T,
-                           XOR_output[i],
-                           10000)
-        HiddenNeuron0.train()
-        HiddenNeuron0.train()
-
-for i in XOR_input:
-    print(OutputNeuron.calc(np.array([HiddenNeuron0.calc(i), HiddenNeuron1.calc(i)]).T))
-
-# print(HiddenNeuron0.calc(XOR_input))
-# print(HiddenNeuron1.calc(XOR_input))
-# HiddenNeuron0.print()
-# HiddenNeuron1.print()
-
-#print(OutputNeuron.calc(np.array([HiddenNeuron0.calc(XOR_input), HiddenNeuron1.calc(XOR_input)])))
+I Wanted to do this OOP but i couldn't get it to work 
+'''
+print("opdracht 4.3")
 
 
+#The input
+XOR_input = np.array([[0,0],
+                      [0,1],
+                      [1,0],
+                      [1,1]])
+#The output
+XOR_output = np.array([[0],[1],[1],[0]])
+
+#random seed
+np.random.seed(2020)
+
+#the weights in a matrix -1 is the bias
+syn0 = 2 * np.random.random((2, 4)) - 1
+syn1 = 2 * np.random.random((4, 1)) - 1
+
+#training
+for i in range(10000):
+
+  #layers
+  layer0 = XOR_input
+  layer1 = sigmoid(np.dot(layer0, syn0))
+  layer2 = sigmoid(np.dot(layer1, syn1))
+
+  #backpropagation
+  layer2_error = XOR_output - layer2
+
+  #Error gets printed
+  if (i % 10000) == 0:
+    print("error " + str(np.mean(np.abs(layer2_error))))
+
+  layer2_delta = layer2_error * sigmoid(layer2, True)
+
+  layer1_error = layer2_delta.dot(syn1.T)
+
+  layer1_delta = layer1_error * sigmoid(layer1, True)
+
+  #Update the weights
+  syn1 += layer1.T.dot(layer2_delta)
+  syn0 += layer0.T.dot(layer1_delta)
+
+'''Test of the network'''
+layer0 = np.array([[0,1]])
+layer1 = sigmoid(np.dot(layer0, syn0))
+layer2 = sigmoid(np.dot(layer1, syn1))
+print("output is must be 1", layer2)
+
+'''4.4 Iris dataset'''
+print("opdracht4.4")
+file_train = "iris.data"
+file_validate = "bezdekIris.data"
+input = dataReader(file_train)
+output = nameReader(file_train)
+
+syn0 = 2 * np.random.random((4, 4)) - 1
+syn1 = 2 * np.random.random((4, 3)) - 1
+
+#training
+for i in range(1000000):
+
+  #layers
+  layer0 = input
+  layer1 = sigmoid(np.dot(layer0, syn0))
+  layer2 = sigmoid(np.dot(layer1, syn1))
+
+  #backpropagation
+  layer2_error = output - layer2
+
+  #Error gets printed
+  if (i % 1000) == 0:
+    print("error " + str(np.mean(np.abs(layer2_error))))
+
+  layer2_delta = layer2_error * sigmoid(layer2, True)
+
+  layer1_error = layer2_delta.dot(syn1.T)
+
+  layer1_delta = layer1_error * sigmoid(layer1, True)
+
+  #Update the weights
+  syn1 += layer1.T.dot(layer2_delta)
+  syn0 += layer0.T.dot(layer1_delta)
+
+
+#test of the network
+input = dataReader(file_validate)
+output = nameReader(file_validate)
+
+layer0 = input
+layer1 = sigmoid(np.dot(layer0, syn0))
+layer2 = sigmoid(np.dot(layer1, syn1))
+number_of_good = probability(layer2, output)
+print(number_of_good, " of the " ,len(output), " good that is %", number_of_good / len(output) * 100)
+
+'''
+it has now 1000000 iteration and it takes a minut on my laptop for 91% 
+
+
+'''
 
 
 
 
 
-
-
-# a = Neuron(3)
-# a.train(input_vector, output, 1000)
-#
-# a.print()
-# print(a.calc(input_vector))
 
 
 
