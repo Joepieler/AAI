233276e809bb80e2549cca65bcc26f15e153a53c
Joepieler <be.campagne@live.nl>
2020-07-08 17:32:23 +0200

    herkansing begin

---
+++ Herkansing/covers/Image_downloader.py
@@ -0,0 +1,23 @@
+import requests
+import shutil
+import numpy as np
+
+DataSet = "BX-Books.csv"
+
+links =  np.genfromtxt(DataSet, delimiter=";", usecols=(0), loose=False, invalid_raise=False, dtype=str)
+
+for i in range(51747, len(links)):
+    r = requests.get(links[i], stream = True)
+    if r.status_code == 200:
+        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
+        r.raw.decode_content = True
+
+        # Open a local file with wb ( write binary ) permission.
+        with open(str(i) + ".jpg",'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+        print('Image sucessfully Downloaded: ',i)
+    else:
+        print('Image Couldn\'t be retreived')
+
+
